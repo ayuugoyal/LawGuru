c75208676f21e5b3d666b307952bd11a5d6ca967
lunchspider <lunchspider03@gmail.com>
2024-02-01 17:04:50 +0530

    [FEAT]: added message handler

--- src/app/api/start_chat.ts
+++ src/app/api/start_chat.ts
@@ -1,2 +1,17 @@
 'use server';
 
+import { NextResponse } from 'next/server';
+import { auth } from "@clerk/nextjs";
+import { db } from '@/db/index';
+import { chat } from '@/db/schema';
+
+export async function POST() {
+    const { userId: user_id } = auth();
+    if (!user_id) {
+        return new Response('Unauthrized', { status: 401 });
+    }
+
+    const res = await db.insert(chat).values({ user_id, title: '' }).returning();
+
+    return NextResponse.json({ data: res[0] });
+}
