16f81c6e58aa89312509349359453f50b0131565
ayuugoyal <ayushgoyal8178@gmail.com>
2024-02-01 05:37:22 +0530

    feat: add nav and footbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
+import Footer from "@/components/Footer";
 import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex justify-center h-screen items-center">
-      <Link href="/dashboard">
-        <div>Get Started</div>
-      </Link>
+    <div className="h-screen flex justify-between flex-col">
+      <div className="navbar bg-base-100 sm:px-16">
+        <div className="navbar-start">
+          <a className="btn btn-ghost text-xl">LawGuru</a>
+        </div>
+        <div className="navbar-end gap-3">
+          <Link className="btn" href="/sign-up">
+            Sign Up
+          </Link>
+          <Link className="btn" href="/sign-in">
+            Sign In
+          </Link>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
